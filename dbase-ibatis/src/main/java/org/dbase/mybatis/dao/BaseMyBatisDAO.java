9a86806dc100fe954e83b962eba37703a83441b0
donilan <donilan.qq@gmail.com>
2012-09-09 00:21:59 +0800

    add mybatis base dao

---
+++ dbase-ibatis/src/main/java/org/dbase/mybatis/dao/BaseMyBatisDAO.java
@@ -0,0 +1,27 @@
+package org.dbase.mybatis.dao;
+
+import java.util.List;
+
+/**
+ * Base mybatis dao
+ * @author Doni
+ * @since 2012-09-09
+ */
+public interface BaseMyBatisDAO {
+
+	/**
+	 * Insert an object
+	 */
+	int insert(Object o);
+	
+	/**
+	 * update an object
+	 */
+	int update(Object o);
+	
+	int delete(Object o);
+	
+	<T> List<T> queryForList(Object o);
+	
+	<T> List<T> queryForList(Object o, int page, int rows);
+}
---
+++ dbase-ibatis/src/main/java/org/dbase/mybatis/dao/impl/BaseMyBatisDAOImpl.java
@@ -0,0 +1,8 @@
+package org.dbase.mybatis.dao.impl;
+
+import org.dbase.mybatis.dao.BaseMyBatisDAO;
+
+public class BaseMyBatisDAOImpl implements BaseMyBatisDAO {
+
+	
+}
