9c1312ed592b01066ae22de5948c6698acd9d758
donilan <donilan.qq@gmail.com>
2012-09-09 01:14:46 +0800

    change some dao code.

--- dbase-ibatis/src/main/java/org/dbase/mybatis/dao/BaseMyBatisDAO.java
+++ dbase-ibatis/src/main/java/org/dbase/mybatis/dao/BaseMyBatisDAO.java
@@ -12,6 +12,7 @@ public interface BaseMyBatisDAO {
 
 	/**
 	 * Insert an object
+	 * @return success return great than 0, fail return less than 0
 	 */
 	int insert(Object o);
 	
@@ -22,7 +23,52 @@ public interface BaseMyBatisDAO {
 	
 	int delete(Object o);
 	
+	/**
+	 * query an object by id
+	 * @author Doni
+	 * @since 2012-9-9
+	 */
+	<T> T queryForOne(Long id);
+	
+	/**
+	 * count objects from a table
+	 * @author Doni
+	 * @since 2012-9-9
+	 */
+	Long count(Object o);
+	
+	/**
+	 * @see #queryForList(String, Object)
+	 * @author Doni
+	 * @since 2012-9-9
+	 */
 	<T> List<T> queryForList(Object o);
 	
+	/**
+	 * @see #queryForList(String, Object, int, int)
+	 * @author Doni
+	 * @since 2012-9-9
+	 * @return
+	 */
 	<T> List<T> queryForList(Object o, int page, int rows);
+	
+	/**
+	 * 
+	 * @author Doni
+	 * @since 2012-9-9
+	 * @param sqlMapId - sql map id
+	 * @param o - query object
+	 */
+	<T> List<T> queryForList(String sqlMapId, Object o);
+	/**
+	 * 
+	 * @author Doni
+	 * @since 2012-9-9
+	 * @param sqlMapId - sql map id
+	 * @param o - query object
+	 * @param page - page
+	 * @param rows - rows of per page
+	 * @return
+	 */
+	<T> List<T> queryForList(String sqlMapId, Object o, int page, int rows);
 }
