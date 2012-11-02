f260448f31517a04ca7f4b9f4b67b42ad6718ac2
donilan <donilan.qq@gmail.com>
2012-11-02 17:51:15 +0800

    Add an abstract method to AbstractController.

--- dbase-web/src/main/java/com/ii2d/dbase/web/controller/AbstractController.java
+++ dbase-web/src/main/java/com/ii2d/dbase/web/controller/AbstractController.java
@@ -1,8 +1,6 @@
 //~ generate by eclipse
 package com.ii2d.dbase.web.controller;
 
-import javax.annotation.Resource;
-
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,37 +16,41 @@ import com.ii2d.dbase.commons.service.CommonService;
  * 
  */
 public abstract class AbstractController {
-	
+
 	public static final String INSTANCE = "instance";
 
-	@Resource
-	protected CommonService commonService;
-	
 	@RequestMapping(method = RequestMethod.GET, value = "{id}")
 	public String find(@PathVariable java.lang.Integer id, ModelMap model) {
-		model.addAttribute(INSTANCE, commonService.queryForById(id, getInstanceClass()));
+		model.addAttribute(INSTANCE,
+				getCommonService().queryForById(id, getInstanceClass()));
 		return String.format("admin/%s/show", getControllerName());
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/edit/{id}")
 	public String edit(@PathVariable(value = "id") int id, ModelMap model) {
-		model.addAttribute(INSTANCE, commonService.queryForById(id, getInstanceClass()));
+		model.addAttribute(INSTANCE,
+				getCommonService().queryForById(id, getInstanceClass()));
 		return String.format("admin/%s/edit", getControllerName());
 	}
+
 	@RequestMapping(method = RequestMethod.GET, value = "/create")
-	public String create(ModelMap model) throws InstantiationException, IllegalAccessException {
+	public String create(ModelMap model) throws InstantiationException,
+			IllegalAccessException {
 		model.addAttribute(INSTANCE, getInstanceClass().newInstance());
 		return String.format("admin/%s/create", getControllerName());
 	}
-	
+
 	@RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
-	public @ResponseBody String delete(@PathVariable java.lang.Integer id, ModelMap model) {
-		int count = commonService.delete(id, getInstanceClass());
+	public @ResponseBody
+	String delete(@PathVariable java.lang.Integer id, ModelMap model) {
+		int count = getCommonService().delete(id, getInstanceClass());
 		model.addAttribute(count);
 		return "success";
 	}
-	
+
 	public abstract Class<?> getInstanceClass();
-	
+
 	public abstract String getControllerName();
+
+	public abstract CommonService getCommonService();
 }
